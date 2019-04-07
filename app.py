72d210b9977061fb2c3a50532f9c80fceb9058c7
Salvador Hernandez Mendoza <salvadorhm@gmail.com>
2019-04-07 15:35:37 -0500

    Configurar la url remota

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ remote = True
 
 if remote is True: # Config remote database and remote oauth host
     db = database.db_cloud
-    host_config = 'http://REMOTE_SERVER/auth/%s/callback'
+    host_config = 'http://herokuoauth2019.herokuapp.com/auth/google/callback'
 elif remote is False: # Config local database and local oauth host
     db = database.db_localhost
     host_config =  'http://localhost:8080/auth/%s/callback'
